b828d7d3c7aa46f834bdf34553ddc87e4651ed77
nihadmirzazade <13638@holbertonstudents.com>
2026-06-13 19:16:09 +0000

    fix

--- machine_learning/unsupervised_learning/4-agglomerative.py
+++ machine_learning/unsupervised_learning/4-agglomerative.py
@@ -10,24 +10,18 @@ def Agglomerative_Clustering(X, n_clusters, random_state,
                               n_components, use_pca_data=True):
     """Perform agglomerative clustering with optional PCA reduction."""
     if use_pca_data:
-        X_used, _ = Apply_PCA(
-            X,
-            n_components=n_components,
-            random_state=random_state
-        )
+        X_used, _ = Apply_PCA(X, n_components=n_components,
+                               random_state=random_state)
     else:
         X_used = X
 
-    model = cluster.AgglomerativeClustering(
-        n_clusters=n_clusters,
-        linkage='ward'
-    )
+    model = cluster.AgglomerativeClustering(n_clusters=n_clusters,
+                                            linkage='ward')
     model.fit(X_used)
 
-    score = (
-        metrics.silhouette_score(X_used, model.labels_)
-        if n_clusters > 1
-        else None
-    )
+    if n_clusters > 1:
+        score = metrics.silhouette_score(X_used, model.labels_)
+    else:
+        score = None
 
     return model, X_used, score
